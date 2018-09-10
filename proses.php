ca62588f438239831d4f17204424b9b716311b3f
Shintapramuwidya <42725343+Shintapramuwidya@users.noreply.github.com>
2018-09-10 10:05:06 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,36 @@
 <?php 
 //Tuliskan kode anda di bawah ini
+	function rekursif($i, $angka){
+		global $i;
+		rekursif2($i, $angka);
+		echo "<br>";
+		if(--$angka > $i){
+			rekursif($angka);
+		}
+	}
+	function rekursif2($j, $angka){
+		global $i;
+		if ($j > $i - $angka) {
+			echo "*";
+			rekursif2($j - 1, $angka);
+		}
+	}
+	$angka=$_POST['angka'];
+	echo "Angka: $angka<br>";
+	$i=0;
+	rekursif($angka);
 
+	for ($i=1; $i <= $angka; $i++) { 
+		if ($i % 2 == 0) {
+			$genap[]=$i;
+		} else{
+			$ganjil[]=$i;
+		}
+	}
+	foreach ($genap as $bil_genap) {
+		echo $bil_genap.",";
+	}
+	foreach ($ganjil as $bil_ganjil) {
+		echo $bil_genap.",";
+	}
 ?>
